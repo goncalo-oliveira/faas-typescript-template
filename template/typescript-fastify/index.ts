8898e5e420babbd446191a3b4fbf1322973556e8
Goncalo Oliveira <goncalo.a.oliveira@gmail.com>
2021-02-19 10:54:14 +0100

    fixed fastify binding

--- template/typescript-fastify/index.ts
+++ template/typescript-fastify/index.ts
@@ -7,9 +7,9 @@ const listenPort = 9000
 const start = async () => {
     try {
         await func.configure( server )
-            .listen( listenPort )
+            .listen( listenPort, '0.0.0.0' )
     
-        console.log( `[server] listening at http://localhost:${listenPort}` )
+        console.log( `[server] listening at http://0.0.0.0:${listenPort}` )
     
     } catch (err) {
         server.log.error(err)
